3f9f1a77924a26e0fc71f7b2c57208753e31232b
plane000 <playsplane@gmail.com>
2018-04-23 20:05:57 +0100

    Started Calculator

---
+++ C#/WinFormsCalc/Windows Form Calculator/Calculator.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Windows_Form_Calculator {
+    partial class Calculator {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.C1 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // C1
+            // 
+            this.C1.Location = new System.Drawing.Point(12, 107);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(50, 50);
+            this.C1.TabIndex = 0;
+            this.C1.Text = "1";
+            this.C1.UseVisualStyleBackColor = true;
+            this.C1.Click += new System.EventHandler(this.C1_Click);
+            // 
+            // C2
+            // 
+            this.C2.Location = new System.Drawing.Point(77, 107);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(50, 50);
+            this.C2.TabIndex = 1;
+            this.C2.Text = "2";
+            this.C2.UseVisualStyleBackColor = true;
+            this.C2.Click += new System.EventHandler(this.C2_Click);
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(256, 343);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.C1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Calculator";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button C1;
+        private System.Windows.Forms.Button C2;
+    }
+}
+
---
+++ C#/WinFormsCalc/Windows Form Calculator/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Windows_Form_Calculator {
+    public partial class Calculator : Form {
+        public Calculator() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+
+        private void C1_Click(object sender, EventArgs e) {
+            Debug.WriteLine("Button 1");
+        }
+
+        private void C2_Click(object sender, EventArgs e) {
+            Debug.WriteLine("Button 2");
+        }
+    }
+}
