c873e9e9b105d474654e69045b88567bbdf52f3c
plane000 <playsplane@gmail.com>
2018-05-14 17:40:03 +0100

    Bug Fixed DNS Tests

--- C#/Networking Tools/Networking Tools/DNSTest.cs
+++ C#/Networking Tools/Networking Tools/DNSTest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,16 +12,19 @@ namespace Networking_Tools {
 
         public string[] GetIPs() {
             List<string> ipAddresses = new List<string>();
-            IPHostEntry iphost = Dns.GetHostEntry(HostName);
 
-            foreach (IPAddress theAddress in iphost.AddressList) {
-                if (theAddress.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork) {
-                    ipAddresses.Add(theAddress.ToString());
+            try {
+                IPHostEntry iphost = Dns.GetHostEntry(HostName);
+
+                foreach (IPAddress theAddress in iphost.AddressList) {
+                    if (theAddress.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork) {
+                        ipAddresses.Add(theAddress.ToString());
+                    }
                 }
+            } catch (SocketException) {
+                ipAddresses.Add("");
             }
-
             return ipAddresses.ToArray();
         }
-
     }
 }
