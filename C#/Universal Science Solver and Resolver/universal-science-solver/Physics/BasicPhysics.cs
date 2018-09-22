1216d995093f188f3191526ac91279f02ed51953
plane000 <playsplane@gmail.com>
2018-09-22 17:15:04 +0100

    asdf

--- C#/Universal Science Solver and Resolver/universal-science-solver/Physics/BasicPhysics.cs
+++ C#/Universal Science Solver and Resolver/universal-science-solver/Physics/BasicPhysics.cs
@@ -7,7 +7,9 @@ using System.Threading.Tasks;
 namespace universal_science_solver.Physics {
     class BasicPhysics {
         public static void SpeedDistanceTime() {
+            Console.WriteLine();
             Console.WriteLine("Calculate distance, speed or time");
+            Console.Write(">");
 
             
         }
