07ad0adf9c4f732d7d9161f0e8ed5fabd997ddd0
udmsoft <udmsoft@outlook.com>
2018-09-26 05:21:45 -0400

    Added port option

--- JavaScript/nutcounter/server/index.js
+++ JavaScript/nutcounter/server/index.js
@@ -4,25 +4,32 @@ const bodyParser = require('body-parser');
 let app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 let progress = 0;
 
-app.listen(80);
+console.log(process.argv);
+if (process.argv.length >= 3) {
+  app.listen(parseInt(process.argv[2]));
+} else {
+  app.listen(80);
+}
 
 app.use(express.static(__dirname + '/client'));
 app.get('/', (req, res, next) => {
-    res.status(200).sendFile('index.html');
+  res.status(200).sendFile('index.html');
 });
 
 app.get('/progress', (req, res, next) => {
-    res.status(200).end(JSON.stringify({
-        progress: progress
-    }));
+  res.status(200).end(JSON.stringify({
+    progress: progress
+  }));
 });
 
 app.post('/progress', (req, res, next) => {
-    progress = req.body.progress;
-    res.status(200).end(JSON.stringify({
-        status: 'completed'
-    }));
+  progress = req.body.progress;
+  res.status(200).end(JSON.stringify({
+    status: 'completed'
+  }));
 });
